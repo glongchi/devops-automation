0285b82ee33f529a04b64f022dcf4d9deaae2203
glongchi <glongchi@gmail.com>
2020-07-30 11:50:36 -0400

    add install jq step

--- azurecli/network102.sh
+++ azurecli/network102.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
+###########################################################################################
+# az 300 tutorial
+# Longchi Kanouo - @koomzo
 #
-# Execute this directly in Azure Cloud Shell (https://shell.azure.com) by pasting (SHIFT+INS on Windows, CTRL+V on Mac or Linux)
-# the following line (beginning with curl...) at the command prompt (uses shortened URL to this script as committed in GitHub repo)
-# and then replacing the arg <YourFirstNameArg> with your First Name:
-#
-#   curl -sL https://git.io/slathrop-az-hello-arg | bash -s <YourFirstNameArg>
-#
-
-echo Hello $1!
-echo Your Azure Subscriptions are as follows...
-az account list --output table
+#Script Purpose
+# - stop an azure vm (not deallocate)
+###########################################################################################
